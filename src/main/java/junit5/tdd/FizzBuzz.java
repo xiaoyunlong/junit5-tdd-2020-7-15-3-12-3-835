5f8d0fe417b8a4a113107ce27ea5deb9de67e715
LEBRON XIAO (ITA-ISDC-ISD-OOCLL/ZHA) <XIAOLE@OOCL.COM>
2020-07-23 11:46:41 +0800

    [tdd][Lebron] feature:pass should_return_1_when_count_off_given_1

---
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -0,0 +1,8 @@
+package junit5.tdd;
+
+public class FizzBuzz {
+    public String play(int inputNumber){
+
+        return "1";
+    }
+}
---
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+package junit5.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    void should_return_1_when_count_off_given_1(){
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int inputNumber = 1;
+
+        //when
+        String result = fizzBuzz.play(inputNumber);
+
+        //then
+        assertEquals("1", result);
+    }
+}
