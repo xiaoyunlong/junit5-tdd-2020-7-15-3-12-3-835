c35e4ef7e992d7be4096a21239eb3ff92e7f346c
LEBRON XIAO (ITA-ISDC-ISD-OOCLL/ZHA) <XIAOLE@OOCL.COM>
2020-07-23 13:54:48 +0800

    [tdd][Lebron] refrctor: should return fizzbuzzwhizz when given the inputnumber mutily of 3 , 5 and 7

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -2,6 +2,10 @@ package junit5.tdd;
 
 public class FizzBuzz {
     public String play(int inputNumber) {
+        if (inputNumber % 3 == 0 && inputNumber % 5 == 0 && inputNumber % 7 == 0) {
+            return "fizzbuzzwhizz";
+        }
+
         if (inputNumber % 3 == 0 && inputNumber % 5 == 0) {
             return "fizzbuzz";
         }
