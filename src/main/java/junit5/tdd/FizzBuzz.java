ba8c869d23f5f892fb3eb0ba64c71e53328a794e
LEBRON XIAO (ITA-ISDC-ISD-OOCLL/ZHA) <XIAOLE@OOCL.COM>
2020-07-23 13:35:52 +0800

    [tdd][Lebron] test: should_return_whizz_when_count_off_given_7

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -1,11 +1,15 @@
 package junit5.tdd;
 
 public class FizzBuzz {
-    public String play(int inputNumber){
-        if(inputNumber % 3 == 0){
+    public String play(int inputNumber) {
+        if (inputNumber % 3 == 0 && inputNumber % 5 == 0) {
+            return "fizzbuzz";
+        }
+
+        if (inputNumber % 3 == 0) {
             return "fizz";
         }
-        if(inputNumber % 5 == 0){
+        if (inputNumber % 5 == 0) {
             return "buzz";
         }
         return String.valueOf(inputNumber);
